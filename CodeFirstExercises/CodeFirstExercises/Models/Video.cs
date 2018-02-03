1b427f14a32ab4d7863f70bf03dbbbba783343cf
thenewstockton <phillip5914619@gmail.com>
2018-02-03 11:13:17 +0800

    exercises

---
+++ CodeFirstExercises/CodeFirstExercises/Models/Video.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstExercises.Models
+{
+    public enum Classification
+    {
+        Silver,
+        Gold,
+        Platinum
+    }
+
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public Genre Genre { get; set; }
+        public Classification Classification { get; set; }
+    }
+}
---
+++ CodeFirstExercises/CodeFirstExercises/VidzyContext.cs
@@ -0,0 +1,21 @@
+﻿using CodeFirstExercises.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstExercises
+{
+    public class VidzyContext : DbContext
+    {
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Video> Videos { get; set; }
+
+        public VidzyContext()
+            : base("name=DefaultConnection")
+        {
+        }
+    }
+}
